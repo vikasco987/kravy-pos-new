bf468bce02369c1ba4912521b8292eee56b1daeb
vikas <vikasvikas9095@gmail.com>
2026-07-14 17:51:52 +0530

    fix: update firebase admin config

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,16 +4,25 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app';
 // @ts-ignore
 if (!getApps().length) {
   try {
-    const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT;
-    if (serviceAccountStr) {
-      const serviceAccount = JSON.parse(serviceAccountStr);
+    const projectId = process.env.FIREBASE_PROJECT_ID;
+    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+    // Handle Vercel newlines securely
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, '').replace(/'/g, '')
+      : undefined;
+
+    if (projectId && clientEmail && privateKey) {
       // @ts-ignore
       initializeApp({
         // @ts-ignore
-        credential: cert(serviceAccount),
+        credential: cert({
+          projectId,
+          clientEmail,
+          privateKey,
+        }),
       });
     } else {
-      console.log('Firebase admin initialization error: FIREBASE_SERVICE_ACCOUNT is missing in .env');
+      console.log('Firebase admin initialization error: Missing Variables in .env');
     }
   } catch (error: any) {
     console.log('Firebase admin initialization error', error.stack);
